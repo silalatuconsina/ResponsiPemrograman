d670d9b2909fdeb789ed4ac2e6bb851d38f71ba5
silalatuconsina <sitiqalamlatuconsina@students.amikom,ad\c.id>
2022-04-20 09:38:49 +0700

    inisialisasi project

---
+++ Karyawan.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4405
+{
+    class Karyawan
+    {
+        // constructor default
+        public Karyawan()
+        {
+
+        }
+        // overload constructor 
+        public Karyawan(string nik, string nama, int gajiperbulan)
+        {
+            NIK = nik;
+            NAMA = nama;
+            Gajiperbulan = gajiperbulan;
+        }
+
+        // properties
+        public string NAMA { get; set; }
+        public string NIK { get; set; }
+        public int Gajiperbulan { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ResponsiPemrograman4405
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // membuat objek Karyawan 1
+            Karyawan karyawan1 = new Karyawan();
+
+            // memberi nilai property
+            karyawan1.NIK = "21114424";
+            karyawan1.NAMA = "Muqsid";
+            karyawan1.Gajiperbulan = 3000000;
+
+            // validasi gaji karyawan1
+            if (karyawan1.Gajiperbulan < 0)
+            {
+                karyawan1.Gajiperbulan = 0;
+            }
+
+            // membuat objek Karyawan 2
+            Karyawan karyawan2 = new Karyawan();
+
+            // memberi nilai property 2
+            karyawan2.NIK = "21114405";
+            karyawan2.NAMA = "Sitiqalam";
+            karyawan2.Gajiperbulan = 2000000;
+
+            // validasi gaji karyawan1
+            if (karyawan2.Gajiperbulan < 0)
+            {
+                karyawan2.Gajiperbulan = 0;
+            }
+
+            // mengambil nilai dengan property
+            Console.WriteLine("No Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("---------------------------------------");
+
+            Console.WriteLine("1. {0} {1}\t{2}", karyawan1.NIK, karyawan1.NAMA, karyawan1.Gajiperbulan);
+            Console.WriteLine("2. {0} {1}\t{2}", karyawan2.NIK, karyawan2.NAMA, karyawan2.Gajiperbulan);
+            Console.WriteLine("\n");
+
+            Console.WriteLine("Asyiiiiik kenaikan gaji 10%!! ");
+            Console.WriteLine("\n");
+
+            Console.WriteLine("No Nik/Nama\t\tGaji Bulanan");
+            Console.WriteLine("---------------------------------------");
+
+            Console.WriteLine("1. {0} {1}\t{2}", karyawan1.NIK, karyawan1.NAMA, karyawan1.Gajiperbulan + (karyawan1.Gajiperbulan * 0.1));
+            Console.WriteLine("2. {0} {1}\t{2}", karyawan2.NIK, karyawan2.NAMA, karyawan2.Gajiperbulan + (karyawan2.Gajiperbulan * 0.1));
+
+            Console.ReadKey();
+        }
+    }
+}
